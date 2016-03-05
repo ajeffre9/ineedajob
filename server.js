00db9acf969cf20b38b1a25fe991f45f31b4dbe5
ajeffre9 <ajeffre9@wo.ca>
2016-03-05 05:00:16 -0500

    Got the front end working

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure\n');
-}).listen(port);
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) { 
+        response.writeHeader(200, {"Content-Type": "text/html"});  // <-- HERE!
+        response.write(html);  // <-- HERE!
+        response.end();  
+    }).listen(1337, '127.0.0.1');
+});
